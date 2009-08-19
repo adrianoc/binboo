bb6e43096165a6a22210f8b06301354f099fd53a
Adriano Carlos Verona <adriano.verona@gmail.com>
2009-08-19 00:59:39 -0300

    Set iteration command and tests

---
+++ Vagabot.Tests/Commands/SetIterationCommandTestCase.cs
@@ -0,0 +1,74 @@
+﻿/**
+ * Copyright (c) 2009 Adriano Carlos Verona
+ * 
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ * 
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ * 
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ **/
+
+using NUnit.Framework;
+
+namespace Binboo.Tests.Commands
+{
+	[TestFixture]
+	public partial class SetIterationCommandTestCase : JiraCommandTestCaseBase
+	{
+		[Test]
+		public void TestSimple()
+		{
+			AssertSetIteration("ITBS-001", 42);
+		}
+
+		[Test]
+		public void TestBulkSetIteration()
+		{
+			AssertSetIteration("ITBS-001, ITBS-002, ITBS-003", 42);
+		}
+
+		[Test]
+		public void TestTooManyArguments()
+		{
+			AssertInvalidArguments(
+				"SetIteration: Unmached arguments: \r\n\r\nITBS-005 42 ** NOT EXPECTED **\r\n           ^^^^^^^^^^^^^^^^^^^",
+				"ITBS-005", "42", "** NOT EXPECTED **");
+		}
+		
+		[Test]
+		public void TestInvalidIteration()
+		{
+			AssertInvalidArguments(
+				"SetIteration: Unmached arguments: \r\n\r\nITBS-005 fortytwo\r\n         ^^^^^^^^",
+				"ITBS-005", "fortytwo");
+		}
+		
+		[Test]
+		public void TestInvalidIssueInBulkOperation()
+		{
+			AssertInvalidArguments(
+				@"SetIteration: Argument index 0 (vale = 'ITBS-005, ') is invalid (Validator: ((?<issues>(?:\b(?<param>[A-Za-z]{1,4}-[0-9]{1,4})\s*,?\s*)+\b),required)).",
+				"ITBS-005, WHAT?", "42");
+		}
+
+		[Test]
+		public void TestInvalidIssue()
+		{
+			AssertInvalidArguments(
+				@"SetIteration: Argument index 0 (vale = '42') is invalid (Validator: ((?<issues>(?:\b(?<param>[A-Za-z]{1,4}-[0-9]{1,4})\s*,?\s*)+\b),required)).",
+				"WHAT?", "42");
+		}
+	}
+}
---
+++ Vagabot/Core/Commands/SetIterationCommand.cs
@@ -0,0 +1,63 @@
+﻿/**
+ * Copyright (c) 2009 Adriano Carlos Verona
+ * 
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ * 
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ * 
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ **/
+
+using System;
+using System.Text;
+using Binboo.Core.Commands.Arguments;
+using Binboo.JiraIntegration;
+
+namespace Binboo.Core.Commands
+{
+	internal class SetIterationCommand : JiraCommandBase
+	{
+		public SetIterationCommand(IJiraProxy jira, string help) : base(jira, help)
+		{
+		}
+
+		public override string Id
+		{
+			get { return "SetIteration"; }
+		}
+
+		protected override string ProcessCommand(IContext context)
+		{
+			var arguments = CollectAndValidateArguments(context.Arguments, issueId => ParamValidator.MultipleIssueId, iteration => ParamValidator.Iteration);
+			var sb = new StringBuilder();
+			
+			foreach (var issue in arguments["issueId"].Values)
+			{
+				string currentIssue = issue;
+				sb.AppendLine(Run(
+									() => _jira.UpdateIssue(currentIssue, String.Empty, NewIteration(arguments["iteration"])),
+									string.Format("Iteration set for issue '{0}'.", currentIssue)
+								));
+			}
+
+			return sb.ToString();
+		}
+
+		private static IssueField NewIteration(Argument iteration)
+		{
+			return IssueField.CustomField(CustomFieldId.Iteration) <= iteration.Value;
+		}
+	}
+}
