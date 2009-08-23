78d1bfd89b933b63b13b4bc0c3bd22e2ab367e9d
Adriano Carlos Verona <adriano.verona@gmail.com>
2009-08-23 10:11:45 -0300

    Better code structure + minor bugs fixed + more tests

--- Vagabot.Tests/Commands/IssueAssignCommandTestCase.cs
+++ Vagabot.Tests/Commands/IssueAssignCommandTestCase.cs
@@ -19,18 +19,13 @@
  * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
  * THE SOFTWARE.
  **/
-using System;
-using System.Linq.Expressions;
-using System.Text;
 using Binboo.Core.Commands;
-using Binboo.JiraIntegration;
-using Moq;
 using NUnit.Framework;
 
 namespace Binboo.Tests.Commands
 {
 	[TestFixture]
-	public class IssueAssignCommandTestCase : JiraCommandTestCaseBase
+	public partial class IssueAssignCommandTestCase : JiraCommandTestCaseBase
 	{
 		[Test]
 		public void TestJustRequiredArguments()
@@ -41,100 +36,49 @@ namespace Binboo.Tests.Commands
 		[Test]
 		public void TestBulkAssignment()
 		{
-			AssertIssueAssignment("PRJ-123, PRJ-124, PRJ-125", "binboo_test_user", "42", string.Empty);
+			AssertIssueAssignment("PRJ-123, PRJ-124, PRJ-125", "binboo_test_user", string.Empty, "42");
 		}
 		
 		[Test]
 		public void TestBulkAssignmentWithInvalidIssue()
 		{
-			AssertIssueAssignment("PRJ-123, " +  NonExistingIssue + ", PRJ-125", "binboo_test_user", "42", string.Empty);
+			AssertIssueAssignment("PRJ-123, " + NonExistingIssue + ", PRJ-125", "binboo_test_user", string.Empty, "42");
 		}
 
 		[Test]
 		public void TestFullArgumentLine()
 		{
-			AssertIssueAssignment("FAL-123", "binboo_fal", "12", "fal_peer");
+			AssertIssueAssignment("FAL-123", "binboo_fal", "fal_peer", "12");
 		}
 
 		[Test]
 		public void TestIterationIsSetAfterFirstAssignmetn()
 		{
-			AssertIssueAssignment("PRJ-123", "binboo_test_user", "42", string.Empty);
+			AssertIssueAssignment("PRJ-123", "binboo_test_user", string.Empty, "42");
 			AssertIssueAssignment("PRJ-123", "binboo_test_user", p => p.Values[0] == "42");
 		}
 
-		private void AssertIssueAssignment(string ticket, string user, Expression<Predicate<IssueField>> iterationPredicate)
+		[Test]
+		public void TestAssigneePeerAreCached()
 		{
-			using (var commandMock = NewCommand<IssueAssignCommand>(
-				mock => mock.Setup(proxy => proxy.AssignIssue(
-												ticket,
-												It.Is<IssueField>(assigneeField => assigneeField.Id == IssueField.Assignee.Id),
-												It.Is<IssueField>(peerField => peerField.Id == CustomFieldId.Peers.Id),
-												It.Is(iterationPredicate)
-												))))
-			{
+			AssertIssueAssignment("user-1", "PRJ-123", "woody", "rex", "42");
+			AssertIssueAssignment("user-2", "PRJ-171", "andy", "sid", "42");
+			AssertIssueAssignment("user-3", "STOP-174", "boo", "mikewazowski", "42");
 
-				IContext context = ContextMockFor(ticket, user).Object;
-				string result = commandMock.Process(context);
-
-				Assert.AreEqual(ExpectedAssignmentMessageFor(ticket, user), result);
-				commandMock.Verify();
-			}
+			AssertCachedAssigneeAndPeer("user-1", "PRJ-202", "woody", "rex");
+			AssertCachedAssigneeAndPeer("user-2", "PRJ-303", "andy", "sid");
+			AssertCachedAssigneeAndPeer("user-3", "PRJ-174", "boo", "mikewazowski");
+			AssertCachedAssigneeAndPeer("user-2", "PRJ-303", "andy", "sid");
+			AssertCachedAssigneeAndPeer("user-1", "PRJ-202", "woody", "rex");
 		}
 
-		private static string ExpectedAssignmentMessageFor(string ticketList, string user)
+		[Test]
+		public void TestNoAssigneePeerCached()
 		{
-			var sb = new StringBuilder();
-			foreach (var ticket in ticketList.Split(','))
-			{
-				var ticketWithNoSpaces = ticket.Trim();
-				if (NonExistingIssue == ticketWithNoSpaces)
-				{
-					sb.AppendFormat("Failed to asssign issue {0}\r\nFailed to get issue: {0}\r\n", ticketWithNoSpaces);
-				}
-				else
-				{
-					sb.AppendFormat("Successfuly assigned issue {0} to {1}\r\n", ticketWithNoSpaces, user);
-				}
-			}
-
-			return sb.Remove(sb.Length - 2, 2).ToString();
+			Reset<IssueAssignCommand>();
+			AssertNoAssigneeAndPeerCached("user-1", "PRJ-202");
+			AssertNoAssigneeAndPeerCached("user-1", "PRJ-202, PRJ-303");
 		}
-
-		private void AssertIssueAssignment(string ticket, string user, string iteration, string peer)
-		{
-			CommandMock<IssueAssignCommand> commandMock = NewCommand<IssueAssignCommand>(MockSetupsFor(ticket, peer));
-
-			IContext context = ContextMockFor(ticket, user, peer, iteration).Object;
-			string result = commandMock.Process(context);
-			Assert.AreEqual(ExpectedAssignmentMessageFor(ticket, user), result);
-		}
-
-		private static Action<Mock<IJiraProxy>>[] MockSetupsFor(string ticket, string peer)
-		{
-			string[] ticketList = ticket.Split(',');
-			var setups = new Action<Mock<IJiraProxy>>[ticketList.Length];
-
-			for(int i =0; i < ticketList.Length; i++)
-			{
-				var currentTicket = ticketList[i].Trim();
-				setups[i] = mock =>
-				{
-					var setup = mock.Setup(proxy => proxy.AssignIssue(
-					                    	currentTicket,
-					                    	It.Is<IssueField>(issueField => issueField.Id == IssueField.Assignee.Id),
-					                    	It.Is<IssueField>(p => p.Id == CustomFieldId.Peers.Id && (string.IsNullOrEmpty(peer) || p.Values[0] == peer)),
-					                    	It.Is<IssueField>(p => p == null || p.Id == CustomFieldId.Iteration.Id)));
-
-					if (NonExistingIssue == currentTicket)
-					{
-						setup.Throws(new JiraProxyException("Failed to asssign issue " + NonExistingIssue, new JiraProxyException("Failed to get issue: " + NonExistingIssue)));
-					}
-				};
-			}
-
-			return setups;
- 		}
 
 		private const string NonExistingIssue = "NON-666";
 	}
