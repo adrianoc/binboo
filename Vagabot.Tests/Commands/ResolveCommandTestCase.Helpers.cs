69cb394ca666bab25fb1d6fefe8a4a575f14bd20
Adriano Carlos Verona <adriano.verona@gmail.com>
2010-02-24 18:25:28 -0300

    A 'resolve' command would be to similar to the actual 'close' command. Since, no one is using close, I decided to rename it to resolve and apply the required changes. Not working yet

---
+++ Vagabot.Tests/Commands/ResolveCommandTestCase.Helpers.cs
@@ -0,0 +1,70 @@
+﻿/**
+ * Copyright (c) 2009 Adriano Carlos Verona
+ * 
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ * 
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ * 
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ **/
+
+using System;
+using Binboo.Core.Commands;
+using Binboo.JiraIntegration;
+using Moq;
+using NUnit.Framework;
+
+namespace Binboo.Tests.Commands
+{
+	public partial class ResolveCommandTestCase
+	{
+		private void AssertResolve(string ticket, IssueResolution resolution)
+		{
+			AssertResolve(ticket, resolution, String.Empty);
+		}
+
+		private void AssertResolve(string ticket, IssueResolution resolution, string comment)
+		{
+			AssertResolve(ticket, resolution, comment, string.Empty);
+		}
+
+		private void AssertResolve(string ticket, IssueResolution resolution, string comment, string fixedInVersions)
+		{
+			string noQuotesComment = StripQuotes(comment);
+			using (var commandMock = NewCommand<ResolveIssueCommand>(ExpectedMethodCalls(ticket, noQuotesComment, resolution, fixedInVersions)))
+			{
+				var contextMock = ContextMockFor("resolving-user", String.Format("{0} \"{1}\"{2}", ticket, resolution.Description.ToLower(), String.IsNullOrEmpty(comment) ? "" : (" " + comment)));
+				contextMock.Setup(ctx => ctx.UserName).Returns("unit.test.user");
+
+				Assert.AreEqual("OK", commandMock.Process(contextMock.Object));
+			}
+		}
+
+		private Action<Mock<IJiraProxy>> ExpectedMethodCalls(string ticket, string noQuotesComment, IssueResolution resolution, string fixedInVersions)
+		{
+			return proxyMock => proxyMock.Setup(
+									p => p.ResolveIssue(
+												ticket, 
+												It.Is<String>(remmark => remmark == noQuotesComment), 
+												resolution, 
+												It.Is<RemoteVersion[]>(versions => fixedInVersions.Length > 0)));
+		}
+
+		private static string StripQuotes(string comment)
+		{
+			return comment.Replace("\"", "");
+		}
+	}
+}
---
+++ Vagabot/Core/Commands/ResolveIssueCommand.cs
@@ -0,0 +1,71 @@
+/**
+ * Copyright (c) 2009 Adriano Carlos Verona
+ * 
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ * 
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ * 
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ **/
+
+using System;
+using System.Collections.Generic;
+using Binboo.Core.Commands.Arguments;
+using Binboo.JiraIntegration;
+
+namespace Binboo.Core.Commands
+{
+	internal class ResolveIssueCommand : JiraCommandBase
+	{
+		public ResolveIssueCommand(IJiraProxy proxy, string help) : base(proxy, help)
+		{
+		}
+
+		public override string Id
+		{
+			get { return "Resolve"; }
+		}
+
+		protected override string ProcessCommand(IContext context)
+		{
+			IDictionary<string, Argument> arguments = CollectAndValidateArguments(context);
+			return CloseIssue(
+						arguments["issueId"],
+						OptionalArgumentOrDefault(arguments, "comment", string.Empty),
+						arguments["resolution"]);
+		}
+
+		private IDictionary<string, Argument> CollectAndValidateArguments(IContext context)
+		{
+			return CollectAndValidateArguments(context.Arguments, 
+			                                   issueId => ParamValidator.IssueId, 
+			                                   resolution => ParamValidator.From(IssueResolution.FriendlyNames()) , 
+			                                   comment => ParamValidator.Anything.AsOptional());
+		}
+
+		private string CloseIssue(string ticket, string comment, string resolution)
+		{
+			try
+			{
+				_jira.ResolveIssue(ticket, comment, IssueResolution.Parse(resolution), new RemoteVersion[0]);
+				return "OK";
+			}
+			catch(JiraProxyException jipe)
+			{
+				return jipe.Message + Environment.NewLine + jipe.InnerException.Message;
+			}
+		}
+	}
+}
