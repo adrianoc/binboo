cb5df4fd989d49a1703153fa3256fac48c2b4b35
Adriano Carlos Verona <adriano.verona@gmail.com>
2010-05-21 00:04:24 -0300

    Unused using

--- Vagabot.Tests/StorageIntegrationTestcase.cs
+++ Vagabot.Tests/StorageIntegrationTestcase.cs
@@ -19,7 +19,6 @@
  * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
  * THE SOFTWARE.
  **/
-using System;
 using System.Linq;
 using Binboo.Core;
 using Binboo.Core.Commands;
