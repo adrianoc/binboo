7ee777465821f2b155f167ce179fbf4d5eda9288
Adriano Carlos Verona <adriano.verona@gmail.com>
2011-10-06 23:11:31 -0300

    Binboo.Tests do not need access to Binboo.Jira internal types

--- Binboo.Jira/Properties/AssemblyInfo.cs
+++ Binboo.Jira/Properties/AssemblyInfo.cs
@@ -35,6 +35,5 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyVersion("1.0.0.0")]
 [assembly: AssemblyFileVersion("1.0.0.0")]
 
-[assembly: InternalsVisibleTo("Binboo.Tests")]
 [assembly: InternalsVisibleTo("Binboo.Jira.Tests")]
 [assembly: InternalsVisibleTo("DynamicProxyGenAssembly2")]
